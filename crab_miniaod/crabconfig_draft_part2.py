cf04dc3a2e894b262b20845f6f3b869018c2c5f8
Deepak Kumar <dekumar@Deepaks-iMac.local>
2018-04-04 18:27:26 +0530

    added crabminiaod

---
+++ crab_miniaod/crabconfig_draft_part2.py
@@ -0,0 +1,11 @@
+')
+# ]
+
+config.section_("Site")
+config.Site.storageSite = 'T2_IN_TIFR'
+config.Site.whitelist = ['T2_*']
+
+#config.section_("User")
+## Only german users
+#config.User.voGroup = "dcms"
+
